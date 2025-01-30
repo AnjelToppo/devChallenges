a29e3135f0f35a3ee61ad84f99b116654ae78650
Anjel-Toppo <anjeltoppo711@gmail.com>
2025-01-30 16:01:53 +0530

    Complete congratulations card feature

---
+++ 4-frontend-libraries-react/7-country-quiz/src/components/Congratulations.jsx
@@ -0,0 +1,12 @@
+import congratsImg from './../assets/congrats.png';
+
+export default function Congratulations() {
+    return (
+        <div className="congratulations">
+            <img className="congratulations__image" src={congratsImg} alt="confetti cannons"/>
+            <h1 className="congratulations__message">Congrats! You completed the quiz.</h1>
+            <p className="congratulations__score">You answer 4/10 correctly</p>
+            <button className="btn play-again">Play again</button>
+        </div>
+    )
+}
