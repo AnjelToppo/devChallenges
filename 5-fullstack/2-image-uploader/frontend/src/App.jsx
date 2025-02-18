e6aa3c5d6a71b821592abd7c6a56573cff6f2bd6
Anjel-Toppo <anjeltoppo711@gmail.com>
2025-02-18 22:13:31 +0530

    Add '/' home routing for project image uploader

--- 5-fullstack/2-image-uploader/frontend/src/App.jsx
+++ 5-fullstack/2-image-uploader/frontend/src/App.jsx
@@ -1,19 +1,12 @@
-import Header from "./components/Header.jsx";
-import {useState} from "react";
-import Main from "./components/Main.jsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+
+
+
+const router = createBrowserRouter([{path: '/', element: <Home />}])
 
 function App() {
-    const [theme, setTheme] = useState("light");
-    function handleThemeClick() {
-        theme === "light" ? setTheme("dark") : setTheme("light");
-    }
-
-  return (
-    <>
-        <Header theme={theme} onThemeClick={handleThemeClick} />
-        <Main theme={theme} />
-    </>
-  )
+    return <RouterProvider router={router}/>
 }
 
 export default App
